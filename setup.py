d28e0994b001afb25bf0ea92fbde4f045cd476bb
Robin Winslow <robin@robinwinslow.co.uk>
2020-02-05 09:47:18 +0000

    v0.2.1: Fix error with lacking htmlSnippet

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="canonicalwebteam.search",
-    version="0.2.0",
+    version="0.2.1",
     author="Canonical webteam",
     author_email="webteam@canonical.com",
     url="https://github.com/canonical-web-and-design/canonicalwebteam.search",
