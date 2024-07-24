5c3388af45d40b9b579f728efdd80fd6b68916c3
Pete <peterfrench94@gmail.com>
2024-07-24 15:01:09 +0200

    chore: Update to use flask_base 2.0.0 which itseld uess Flask 2.3.3

--- setup.py
+++ setup.py
@@ -15,6 +15,6 @@ setup(
     packages=find_packages(),
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    install_requires=["canonicalwebteam.flask-base>=1.1.0", "user-agents>=2.0.0", "Flask-Limiter>=2.9.0"],
+    install_requires=["canonicalwebteam.flask-base>=2.0.0", "user-agents>=2.0.0", "Flask-Limiter>=3.8.0"],
     tests_require=["httpretty"],
 )
