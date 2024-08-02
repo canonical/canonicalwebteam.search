755c97fe4cbd3dc8303847279ff7b817d9d9d8d9
Pete <peterfrench94@gmail.com>
2024-08-02 11:06:51 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="canonicalwebteam.search",
-    version="1.4.0",
+    version="2.0.0",
     author="Canonical webteam",
     author_email="webteam@canonical.com",
     url="https://github.com/canonical/canonicalwebteam.search",
