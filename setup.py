b4b58749c88ea61f3436092499ff228dcb646694
Pete <peterfrench94@gmail.com>
2024-08-02 10:38:03 +0200

    Revert version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="canonicalwebteam.search",
-    version="2.0.0",
+    version="1.4.0",
     author="Canonical webteam",
     author_email="webteam@canonical.com",
     url="https://github.com/canonical/canonicalwebteam.search",
